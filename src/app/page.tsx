b640e6ce79f01934ad3d5c5208f8a360eafbdf3c
joaoBernardinoo <joaobernardino@ufba.br>
2023-11-06 08:53:41 -0300

    feat: vizualização de pedidos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
-"use client";
-import { useState, useEffect } from 'react';
-import './globals.css';
-import Image from 'next/image';
-import logo from "./Logo.png";
-import icon1 from './Icon.png';
-import icon2 from './Group 1.png';
-import icon3 from './Group 2.png';
+'use client'
+import { useState, useEffect } from 'react'
+import './globals.css'
+import Image from 'next/image'
+import logo from '../images/Logo.png'
+import icon1 from '../images/Icon.png'
+import icon2 from '../images/Group 1.png'
+import icon3 from '../images/Group 2.png'
+import FormPedido from '@/components/FormPedido'
 
 interface Pedido {
-  id: number,
-  nome: string,
+  id: number
+  nome: string
   qtd_paes: number
 }
 
 export default function Home() {
-  const [data, setData] = useState<Pedido[]>([]);
-  const [queue, setQueue] = useState<number>(0);
+  const [data, setData] = useState<Pedido[]>([])
+  const [queue, setQueue] = useState<number>(0)
+  const [paes, setPaes] = useState<number>(0)
+  const [faturamento, setFaturamento] = useState<number>(0.0)
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
   useEffect(() => {
     // Simulando uma solicitação GET
@@ -24,19 +28,21 @@ export default function Home() {
     })
       .then((response) => {
         if (response.ok) {
-          return response.json();
+          return response.json()
         } else {
-          throw new Error('Falha na solicitação GET');
+          throw new Error('Falha na solicitação GET')
         }
       })
       .then((responseData) => {
-        setData(responseData.data.data);
-        setQueue(responseData.data.data.length);
+        setData(responseData.data.data)
+        setQueue(responseData.data.data.length)
+        setPaes(data.map((pedido: Pedido) => pedido.qtd_paes).reduce((a, b) => a + b, 0))
+        setFaturamento(paes * 0.45)
       })
       .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+        console.error(error)
+      })
+  }, [data])
 
   const handleDelete = (id: number) => {
     // Simulando uma solicitação DELETE
@@ -50,23 +56,32 @@ export default function Home() {
       .then((response) => {
         if (response.ok) {
           // Atualizar a lista de pedidos após a exclusão
-          setData(data.filter((pedido:Pedido) => pedido.id !== id));
-          setQueue(queue - 1);
+          setData(data.filter((pedido: Pedido) => pedido.id !== id))
+          setFaturamento(
+            data.map((pedido: Pedido) => pedido.qtd_paes * 0.45).reduce((a, b) => a + b, 0)
+          )
+          setPaes(paes - data.filter((pedido: Pedido) => pedido.id === id)[0].qtd_paes)
+          setQueue(queue - 1)
         } else {
-          throw new Error('Falha na solicitação DELETE');
+          throw new Error('Falha na solicitação DELETE')
         }
       })
       .catch((error) => {
-        console.error(error);
-      });
-  };
+        console.error(error)
+      })
+  }
 
-  const handlePost = () => {
+  type requestPedido = {
+    nome: string
+    qtd_paes: number
+  }
+
+  const handlePost = (pedido: requestPedido) => {
     // Simulando uma solicitação POST
     const newPedido = {
-      nome: 'Novo Pedido',
-      qtd_paes: 10,
-    };
+      nome: pedido.nome,
+      qtd_paes: pedido.qtd_paes,
+    }
 
     fetch('/api/pedidos', {
       method: 'POST',
@@ -77,64 +92,71 @@ export default function Home() {
     })
       .then((response) => {
         if (response.ok) {
-          return response.json();
+          return response.json()
         } else {
-          throw new Error('Falha na solicitação POST');
+          throw new Error('Falha na solicitação POST')
         }
       })
       .then((responseData) => {
         // Adicionar o novo pedido à lista de pedidos
-        setData([...data, responseData.data]);
-        setQueue(queue + 1);
+        setData([...data, responseData.data])
+        setFaturamento(faturamento + responseData.data.qtd_paes * 0.45)
+        setQueue(queue + 1)
       })
       .catch((error) => {
-        console.error(error);
-      });
-  };
+        console.error(error)
+      })
+  }
 
   return (
     <main>
-      <div className='header'>
-        <Image className='logo' src={logo} alt="logo"/>
-        <div className='infos'>
-          <section className='tamfila'>
+      {isModalOpen && <FormPedido onClose={() => setIsModalOpen(false)} onAddPedido={handlePost} />}
+      <div className="header">
+        <Image className="logo" src={logo} alt="logo" />
+        <div className="infos">
+          <section className="tamfila">
             <a>
               <div>Pessoas na fila</div>
-              <Image src={icon1} alt="icon1"/>
+              <Image src={icon1} alt="icon1" />
             </a>
             <p>{queue}</p>
           </section>
-          <section className='paesvend'>
+          <section className="paesvend">
             <a>
               <div>Pães vendidos</div>
-              <Image src={icon2} alt="icon2"/>
+              <Image src={icon2} alt="icon2" />
             </a>
-            <p>350</p>
+            <p>{paes}</p>
           </section>
-          <section className='entrada'>
+          <section className="entrada">
             <a>
               <div>Entrada</div>
-              <Image src={icon3} alt="icon3"/>
+              <Image src={icon3} alt="icon3" />
             </a>
-            <p>R$ 175,00</p>
+            <p>R$ {faturamento}</p>
           </section>
         </div>
       </div>
 
-      <div className='body'>
-        <ul>
+      <div className="body">
+        <p className="add" onClick={() => {setIsModalOpen(true)}}>
+          + Adicionar Pedido
+        </p>
+        <div className="pedidos">
           {data.map((pedido: Pedido) => (
-            <li key={pedido.id}>
-              <p>ID: {pedido.id}</p>
-              <p>Nome: {pedido.nome}</p>
-              <p>Quantidade de Pães: {pedido.qtd_paes}</p>
+            <div style={{}}>
+              <div className="card" key={pedido.id}>
+                <p className="nome">{pedido.nome}</p>
+                <p className="desc">
+                  Total de pães: <span className="valor">{pedido.qtd_paes} pães</span> Total a
+                  pagar: <span className="valor">R$ {pedido.qtd_paes * 0.45}</span>
+                </p>
+              </div>
               <button onClick={() => handleDelete(pedido.id)}>Excluir</button>
-            </li>
+            </div>
           ))}
-        </ul>
-        <p className='add' onClick={handlePost}>+ Adicionar Pedido</p>
+        </div>
       </div>
     </main>
-  );
+  )
 }
-
---
+++ src/components/FormPedido.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import './FormPedido.css'
+import { PedidoRequest } from '../../pages/api/models/pedido'
+type FormPedidoProps = {
+  onClose: () => void
+  onAddPedido: (pedido: PedidoRequest) => void
+}
+
+export default function FormPedido({ onClose, onAddPedido }: FormPedidoProps) {
+  const [nome, setNome] = useState('')
+  const [qtdPaes, setQtdPaes] = useState(0)
+
+  const handlePost = () => {
+    // Simulando uma solicitação POST
+    const newPedido = {
+      nome: nome,
+      qtd_paes: qtdPaes,
+    }
+
+    // Chama a função onAddPedido passando o novo pedido
+    onAddPedido(newPedido)
+
+    // Fecha o formulário
+    onClose()
+  }
+
+  return (
+    <div>
+      <div className="blurred-background"></div>
+      <div className="form-container">
+        <h2>Adicionar pessoa à fila</h2>
+        <label>
+          Nome Completo:
+          <input
+            type="text"
+            value={nome}
+            placeholder="Nome completo do cliente"
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </label>
+        <label>
+          Total de Pães:
+          <input
+            type="number"
+            value={qtdPaes}
+            min={0}
+            onChange={(e) => setQtdPaes(Math.max(parseInt(e.target.value)), 0)}
+          />
+        </label>
+        <div className="button-container">
+          <button className="submit-button" onClick={handlePost}>
+            Enviar
+          </button>
+          <button className="cancel-button" onClick={onClose}>
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
